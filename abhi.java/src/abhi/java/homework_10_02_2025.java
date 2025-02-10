afd3dd43c43e37b8f41637e506508067f3ca9f00
LENOVO <LENOVO@DESKTOP-M0901VQ>
2025-02-11 00:14:49 +0530

    homework

---
+++ abhi.java/src/abhi/java/homework_10_02_2025.java
@@ -0,0 +1,14 @@
+package abhi.java;
+import java.util.Scanner;
+
+public class homework_10_02_2025 {
+	public static void main(String[] args) {
+		 Scanner sc= new Scanner(System.in);
+		 int number = sc.nextInt();
+		 System.out.println("enter the number");
+		 
+		 int roundednumber= number - (number%5);
+		 System.out.println("nearest multiple is 5: "+roundednumber);
+	}
+
+}
